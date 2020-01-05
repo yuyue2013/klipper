96becbc0e15a5c68a456a329600a4d7a2869ff4d
Dmitry Butyugin <dmbutyugin@google.com>
2020-01-05 15:13:21 +0500

    moveq: refactor and split into several components
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

---
+++ klippy/chelper/accelgroup.h
@@ -0,0 +1,32 @@
+#ifndef ACCELGROUP_H
+#define ACCELGROUP_H
+
+struct qmove;
+
+// A group of moves accelerating (or decelerating) together
+struct accel_group {
+    int accel_order;
+    double max_accel, min_accel;
+    double max_jerk;
+    double min_jerk_limit_time;
+    double combined_d, accel_d;
+    double accel_t, accel_offset_t, total_accel_t;
+    double uncomp_accel_t, uncomp_accel_offset_t;
+    double start_accel_v;
+    double effective_accel;
+    struct accel_group *start_accel, *next_accel;
+    struct qmove *move;
+    double max_start_v, max_start_v2, max_end_v2;
+};
+
+void fill_accel_group(struct accel_group *ag, struct qmove *m, int accel_order
+                      , double accel, double jerk, double min_jerk_limit_time);
+void limit_accel(struct accel_group *ag, double accel, double jerk);
+void set_max_start_v2(struct accel_group *ag, double start_v2);
+double calc_max_v2(const struct accel_group* ag);
+double calc_effective_accel(const struct accel_group *ag, double cruise_v);
+double calc_min_accel_time(const struct accel_group *ag, double cruise_v);
+double calc_min_accel_dist(const struct accel_group *ag, double cruise_v);
+double calc_min_safe_dist(const struct accel_group *ag, double cruise_v2);
+
+#endif  // accelgroup.h
---
+++ klippy/chelper/trapbuild.h
@@ -0,0 +1,19 @@
+#ifndef TRAPBUILD_H
+#define TRAPBUILD_H
+
+#include "list.h"
+
+struct qmove;
+
+struct vtrap {
+    struct list_head trapezoid;
+    struct qmove *accel_head, *decel_head;
+};
+
+void vtrap_init(struct vtrap *vt);
+struct qmove * vtrap_flush(struct vtrap *vt, struct list_node *next_pos);
+struct qmove * vtrap_clear(struct vtrap *vt, struct list_node *next_pos);
+void vtrap_add_as_accel(struct vtrap *vt, struct qmove *move);
+void vtrap_add_as_decel(struct vtrap *vt, struct qmove *move);
+
+#endif  // trapbuild.h

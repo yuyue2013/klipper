f3af2df68690220d60a0af4a12e15e96e2bb9cc6
Dmitry Butyugin <dmbutyugin@google.com>
2020-05-07 21:41:11 +0200

    kin_extruder: revert changes to extruder smoothing
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/chelper/kin_extruder.c
+++ klippy/chelper/kin_extruder.c
@@ -8,7 +8,6 @@
 #include <stdlib.h> // malloc
 #include <string.h> // memset
 #include "compiler.h" // __visible
-#include "integrate.h" // integrate_weighted
 #include "itersolve.h" // struct stepper_kinematics
 #include "pyhelper.h" // errorf
 #include "trapq.h" // move_get_distance
@@ -22,15 +21,38 @@
 //                       + pressure_advance * nominal_velocity(t))
 // Which is then "smoothed" using a weighted average:
 //     smooth_position(t) = (
-//         definitive_integral(pa_position(x) * w(x-t, smooth_time/2) * dx,
+//         definitive_integral(pa_position(x) * (smooth_time/2 - abs(t-x)) * dx,
 //                             from=t-smooth_time/2, to=t+smooth_time/2)
-//         / definitive_integral(w(x, smooth_time/2)*dx,
-//                               from=-smooth_time/2, to=smooth_time/2)
+//         / ((smooth_time/2)**2))
+
+// Calculate the definitive integral of the motion formula:
+//   position(t) = base + t * (start_v + t * half_accel)
+static double
+extruder_integrate(double base, double start_v, double half_accel
+                   , double start, double end)
+{
+    double half_v = .5 * start_v, sixth_a = (1. / 3.) * half_accel;
+    double si = start * (base + start * (half_v + start * sixth_a));
+    double ei = end * (base + end * (half_v + end * sixth_a));
+    return ei - si;
+}
+
+// Calculate the definitive integral of time weighted position:
+//   weighted_position(t) = t * (base + t * (start_v + t * half_accel))
+static double
+extruder_integrate_time(double base, double start_v, double half_accel
+                        , double start, double end)
+{
+    double half_b = .5 * base, third_v = (1. / 3.) * start_v;
+    double eighth_a = .25 * half_accel;
+    double si = start * start * (half_b + start * (third_v + start * eighth_a));
+    double ei = end * end * (half_b + end * (third_v + end * eighth_a));
+    return ei - si;
+}
 
 // Calculate the definitive integral of extruder for a given move
 static double
-pa_move_integrate(struct move *m, double start, double end, double time_offset
-                  , double hst)
+pa_move_integrate(struct move *m, double start, double end, double time_offset)
 {
     if (start < 0.)
         start = 0.;
@@ -42,7 +64,9 @@ pa_move_integrate(struct move *m, double start, double end, double time_offset
     double start_v = m->start_v + pressure_advance * 2. * m->half_accel;
     // Calculate definitive integral
     double ha = m->half_accel;
-    return integrate_weighted(base, start_v, ha, start, end, time_offset, hst);
+    double iext = extruder_integrate(base, start_v, ha, start, end);
+    double wgt_ext = extruder_integrate_time(base, start_v, ha, start, end);
+    return wgt_ext - time_offset * iext;
 }
 
 // Calculate the definitive integral of the extruder over a range of moves
@@ -51,30 +75,27 @@ pa_range_integrate(struct move *m, double move_time, double hst)
 {
     // Calculate integral for the current move
     double res = 0., start = move_time - hst, end = move_time + hst;
-    double offset = -move_time;
-    res += pa_move_integrate(m, start, end, offset, hst);
+    res += pa_move_integrate(m, start, move_time, start);
+    res -= pa_move_integrate(m, move_time, end, end);
     // Integrate over previous moves
     struct move *prev = m;
     while (unlikely(start < 0.)) {
         prev = list_prev_entry(prev, node);
         start += prev->move_t;
-        offset -= prev->move_t;
-        res += pa_move_integrate(prev, start, prev->move_t, offset, hst);
+        res += pa_move_integrate(prev, start, prev->move_t, start);
     }
     // Integrate over future moves
-    offset = -move_time;
     while (unlikely(end > m->move_t)) {
         end -= m->move_t;
-        offset += m->move_t;
         m = list_next_entry(m, node);
-        res += pa_move_integrate(m, 0., end, offset, hst);
+        res -= pa_move_integrate(m, 0., end, end);
     }
     return res;
 }
 
 struct extruder_stepper {
     struct stepper_kinematics sk;
-    double half_smooth_time, inv_norm;
+    double half_smooth_time, inv_half_smooth_time2;
 };
 
 static double
@@ -88,7 +109,7 @@ extruder_calc_position(struct stepper_kinematics *sk, struct move *m
         return m->start_pos.x + move_get_distance(m, move_time);
     // Apply pressure advance and average over smooth_time
     double area = pa_range_integrate(m, move_time, hst);
-    return area * es->inv_norm;
+    return area * es->inv_half_smooth_time2;
 }
 
 void __visible
@@ -100,7 +121,7 @@ extruder_set_smooth_time(struct stepper_kinematics *sk, double smooth_time)
     es->sk.gen_steps_pre_active = es->sk.gen_steps_post_active = hst;
     if (! hst)
         return;
-    es->inv_norm = calc_inv_norm(hst);
+    es->inv_half_smooth_time2 = 1. / (hst * hst);
 }
 
 struct stepper_kinematics * __visible
